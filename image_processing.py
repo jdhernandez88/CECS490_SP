2399d860e2e02fb3f73c6ce4775599041f074ac3
Jacob <jdhernandez88@yahoo.com>
2016-08-29 08:05:11 -0700

    testing

--- image_processing.py
+++ image_processing.py
@@ -4,20 +4,58 @@ import numpy as np
 img = cv2.imread('hand.jpg')
 
 test = img[0,0]
+#print test
 print test
+# testing some file reading 
+#kernal = open('test.txt', 'r')
+#print kernal.read()
+kernal = [1, 0, 1,
+          0, 1, 0,
+          1, 0, 1]
+
+#multiply each pixial with kernal
+for p in range(0,299)
+for u in range(0,299)
+for l in range(0,4)
+ for o in range(0,9)
+
+    kernal[o]*img[p,u,l]
+q=kernal[0]*image[0,0,0]
+kernal[1]*image[0,1,0]
+kernal[2]*image[0,2,0]
+kernal[3]*image[1,0,0]
+kernal[4]*image[1,1,0]
+kernal[5]*image[1,2,0]
+kernal[6]*image[2,0,0]
+kernal[7]*image[2,1,0]
+kernal[8]*image[2,2,0]
 
 
+#print array1[1]
+s=0
+for z in range(0,3):
+ s = s + kernal[z]
+ print s
 
 #Display Image
 #cv2.imshow('image',img)
-# Scaling factor
+# Scaling factor (fx= , fy= )
 img2 = cv2.resize(img,None,fx=0.10, fy=0.10, interpolation = cv2.INTER_CUBIC)
+print img2[0,0,1]
+print img2 [0,0]
+#255 = black, 0=white
 for x in range(0, 299):
   for y in range (0,299):
-   img2 [x,y] =[255,255,255]
+   img2 [x,y] =[0,255,255]
 
 #Applying Grayscale filter to image
 gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 #Saving filtered image to new file
 cv2.imwrite('graytest.jpg',gray)
+
+#Convolution is simple an element-wise multiplacation of two matrices followed by a sum
+# two matrices, both with the same dimensions
+# multiple them, element by element (not dot product just simple multiplacation)
+# sum the elements together
+
