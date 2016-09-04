74ba20d7615c938367141ef8d791631a6833e6de
Jacob <jdhernandez88@yahoo.com>
2016-09-03 17:24:35 -0700

    Added functions

--- grayscaleTest.py
+++ grayscaleTest.py
@@ -6,52 +6,43 @@ import pprint
 global y
 global img_convo
 
-y = 0
+#Convolution
+kernal = [[0, 1, 10],
+          [-10, -4, 10],
+           [0, 1, 10]]
 
 #Read Image
 original = cv2.imread('hand.jpg')
-img_resize = cv2.resize(original,None,fx=0.10, fy=0.10, interpolation = cv2.INTER_CUBIC)
-
-
-#print (img[0,0,0]+img[0,0,1]+img[0,0,2])//3
-
+image = cv2.resize(original,None,fx=0.10, fy=0.10, interpolation = cv2.INTER_CUBIC)
 
 #Luminance mask algorithm
-for i in range(0,531):
- for j in range(0,299):
-  R = img_resize[i,j,0]*0.299
-  G = img_resize[i,j,1]*0.587
-  B = img_resize[i,j,2]*0.114
-  gray = R+G+B
-  img_resize[i,j] = gray
- 
-img_gray = img_resize
+def grayscale(image):
+    for i in range(0,531):
+     for j in range(0,299):
+      R = image[i,j,0]*0.299
+      G = image[i,j,1]*0.587
+      B = image[i,j,2]*0.114
+      gray = R+G+B
+      image[i,j] = gray
+    cv2.imwrite('graytest.jpg',image)
+    print "Print sent"
+    return image 
+
+def convolution(image):
+    y=0;
+    for i in range(1,530):
+     for j in range(1,298): 
+      for k in range(0,2):
+       for l in range(0,2):
+        x = image[i+k][j+k]*kernal[k][l]
+        y = y + x
+     image[(i+1)][(j+1)] = y  #needs to be fixed
+    cv2.imwrite('convotest.jpg',image)
+
+img_gray = grayscale(image)
+convolution(img_gray)
 
 
-cv2.imwrite('graytest.jpg',img_gray)
-
-
-img_temp = img_gray
-img_convo = img_temp
-
-#Convolution
-kernal = [0, 1, 10,
-          -10, -4, 10,
-         0, 1, 10]
-#print img_convo[1,1,0]
-
-
-for i in range(1,530):
- for j in range(1,298): 
-  for k in range(0,2):
-   for l in range(0,2):
-    x = img_temp[(i+k),(j+k)]*kernal[k+l]
-    y = y + x
- img_convo[(i+1),(j+1)] = y  
-
-cv2.imwrite('convotest.jpg',img_gray)
-
-#pprint.pprint(temp)
 
 
 
